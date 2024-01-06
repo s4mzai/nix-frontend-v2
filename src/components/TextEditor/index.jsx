d880709f527001da365ae49dd2b74d826581b663
Siya <del.siyamittal@gmail.com>
2024-01-06 17:40:39 +0530

    added new story view

--- src/components/TextEditor/index.jsx
+++ src/components/TextEditor/index.jsx
@@ -1,7 +1,8 @@
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
+import './index.css';
 
-export const TextEditor = () => {
+export const TextEditor = (value, onChange) => {
     return <CKEditor
         editor={ ClassicEditor }
         config={{
@@ -12,12 +13,16 @@ export const TextEditor = () => {
             ],
             placeholder: "Type your content here."
         }}  
+        data={value}
+        onChange={(event, editor) => {
+            const data = editor.getData();
+            onChange(data);
+        }}
+        
         onReady={ editor => {
             console.log( 'Editor is ready to use!', editor );
-        } }
-        onChange={ ( event ) => {
-            console.log( event );
-        } }
+            //editor.ui.view.editable.element.style.minHeight = "150px";
+        }}
         onBlur={ ( event, editor ) => {
             console.log( 'Blur.', editor );
         } }
