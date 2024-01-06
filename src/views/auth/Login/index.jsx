29efea046b4c40171ddf0ecb9d6505ab82cb26db
Siya <del.siyamittal@gmail.com>
2024-01-06 22:56:56 +0530

    fixed minor mistakes

--- src/views/auth/Login/index.jsx
+++ src/views/auth/Login/index.jsx
@@ -1,5 +1,5 @@
 import dtutimesIcon from '../../assets/dtutimesIcon.svg'
-export default function Example() {
+export default function Login() {
     return (
       <>
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
