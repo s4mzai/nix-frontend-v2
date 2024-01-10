99fcbc37b169d868ec71ff5ab1b620fff09939f5
Siya <del.siyamittal@gmail.com>
2024-01-10 22:42:44 +0530

    added error page for wrong route and basic routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { Outlet } from 'react-router-dom';
 import './App.css'
+import { AppRoutes } from '@/routes';
+
 
 function App() {
   return (
     <>
+      hi this is root component (for now)
+      <Outlet/>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom";
 
+import {AppRoutes} from '@/routes/index.jsx';
+import '@/index.css'
+
+const router = createBrowserRouter(AppRoutes)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
