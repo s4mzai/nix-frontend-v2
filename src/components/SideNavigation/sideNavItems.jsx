94dde328df05229130c7fe696d5079338db88139
Vishal <dvishal485@gmail.com>
2024-02-13 10:08:15 +0530

    menu updated by Advitiya
    Signed-off-by: Vishal <dvishal485@gmail.com>

---
+++ src/components/SideNavigation/sideNavItems.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+import DownArrow from '@/assets/ChevronDownIcon';
+// import PropTypes from "prop-types"; // Import PropTypes for prop type validation
+import { Link } from "react-router-dom";
+
+function SidebarItem({ items }) {
+    const [open, setOpen] = useState(false);
+    if (items.submenuItems) {
+        return (
+            // make this div appear in full screen
+            <div className="flex flex-col">
+                <div
+                    className="flex items-center justify-between text-white p-2 cursor-pointer"
+                    onClick={() => setOpen(!open)}
+                >
+                    <div className="flex items-center">
+                        <span>{items.label}</span>
+                    </div>
+                    <div>{open ? <DownArrow /> : <DownArrow />}</div>
+                </div>
+                <div className={`${open ? "block" : "hidden"} pl-4`}>
+                    {items.submenuItems.map((item, index) => (
+                        <SidebarItem key={index} items={item} />
+                    ))}
+                </div>
+            </div>
+        );
+    } else {
+        return (
+            <Link to={items.href}>
+                <div className="flex items-center text-white p-2 cursor-pointer hover:bg-gray-500 hover:rounded">
+                    <span>{items.label}</span>
+                </div>
+            </Link>
+        );
+    }
+}
+
+export default SidebarItem;
