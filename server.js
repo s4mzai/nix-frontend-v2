981e17d72eca344b2160b6b0a99368302c966f3b
Vishal <dvishal485@gmail.com>
2024-02-14 13:00:14 +0530

    huh, fix hardcoding my system path
    Signed-off-by: Vishal <dvishal485@gmail.com>

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 import express from 'express'
+import path from 'path';
 const server = express()
+
+const __dirname = path.resolve(path.dirname(''));
 
 server.use('/public', express.static('/public'))
 
 server.get('/assets/*', (req, res) => {
-  res.sendFile('/home/dvishal485/Projects/nix_frontend/dist/assets/' + req.params[0]);
+  const addr = req.params[0];
+  if (!addr) {
+    return res.status(404).send('Not found')
+  }
+  const path_addr = path.join(__dirname, '/dist/assets/', addr);
+  res.sendFile(path_addr);
 })
 
 server.get('/*', (req, res) => {
-  res.sendFile('/home/dvishal485/Projects/nix_frontend/dist/index.html');
+  res.sendFile(path.join(__dirname, '/dist/index.html'));
 })
 
 const port = 5173;
