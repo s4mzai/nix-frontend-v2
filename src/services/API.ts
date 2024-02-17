6669f091a16de3de5036a07a4f52cba2d10bf610
Vishal <dvishal485@gmail.com>
2024-02-17 12:22:18 +0530

    and yes, ofc use parser whenever can be used
    Signed-off-by: Vishal <dvishal485@gmail.com>

--- src/services/API.ts
+++ src/services/API.ts
@@ -1,10 +1,12 @@
 import axios from "axios";
+import { getTokenFromStorage } from "./localStorageParser";
 
 const API = axios.create({ baseURL: import.meta.env.VITE_NIX_BACKEND });
 
 API.interceptors.request.use((req) => {
-  if (localStorage.getItem("token")) {
-    req.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
+  const token = getTokenFromStorage();
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
   }
   return req;
 });
