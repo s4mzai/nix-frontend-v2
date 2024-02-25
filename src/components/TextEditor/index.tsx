affd57aa3d72d042aae18d69bcaa7b0fe45bb5cb
advitiya7 <advitiya3@gmail.com>
2024-02-26 00:59:24 +0530

    heading and paragraph

--- src/components/TextEditor/index.tsx
+++ src/components/TextEditor/index.tsx
@@ -1,23 +1,24 @@
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
-import { CKEditor } from "@ckeditor/ckeditor5-react";
+import  {CKEditor}  from "@ckeditor/ckeditor5-react";
 import "./index.css";
-
 
 const TextEditor = ({value, onChange}) => {
   return <CKEditor
     editor={ ClassicEditor }
     config={{
-      toolbar: [
-        "undo", "redo", "|", "heading", "|",
-        "bold", "italic", "link",
-        "bulletedList", "numberedList", "blockQuote"
-      ],
-      placeholder: "Type your content here."
-    }}  
+      toolbar: {
+        items:[
+          "undo", "redo", "|", "heading", "|",
+          "bold", "italic", "link",
+          "bulletedList", "numberedList", "blockQuote"
+        ],
+      },
+      placeholder: "Type your content here.",
+    }}
     data={value}
     onChange={(event, editor) => {
       const data = editor.getData();
-      onChange(data);
+      onChange(data); 
     }}
         
     onReady={ editor => {
