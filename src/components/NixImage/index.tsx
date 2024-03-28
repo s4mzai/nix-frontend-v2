0674d3d2ea3745a50e2fc2be671507ff0c83fffd
Vishal <dvishal485@gmail.com>
2024-03-29 01:52:20 +0530

    pretty
    Signed-off-by: Vishal <dvishal485@gmail.com>

--- src/components/NixImage/index.tsx
+++ src/components/NixImage/index.tsx
@@ -2,6 +2,7 @@ import API from "@/services/API";
 import React, { useEffect } from "react";
 import { Spinner } from "../Spinner";
 import NixImageProps from "@/types/nixImageProps";
+import ErrorIcon from "@/assets/errorIcon";
 
 export const NixImage: React.FC<NixImageProps> = ({ image_id, ...props }) => {
   const [image, setImage] = React.useState<string | ArrayBuffer>(null);
@@ -18,8 +19,8 @@ export const NixImage: React.FC<NixImageProps> = ({ image_id, ...props }) => {
         image_endpoint.concat(`?t=${new Date().getTime()}`);
       }
     }
-    API.get(image_endpoint, { responseType: "arraybuffer" }).then(
-      (response) => {
+    API.get(image_endpoint, { responseType: "arraybuffer" })
+      .then((response) => {
         const imageBlob = new Blob([response.data], { type: "image/png" });
         const reader = new FileReader();
         reader.onload = () => {
@@ -27,8 +28,11 @@ export const NixImage: React.FC<NixImageProps> = ({ image_id, ...props }) => {
           setImage(imageDataUrl);
         };
         reader.readAsDataURL(imageBlob);
-      },
-    );
+      })
+      .catch((error) => {
+        console.error(error);
+        setImage("");
+      });
   }, [image_id, props]);
 
   if (!image)
@@ -36,8 +40,16 @@ export const NixImage: React.FC<NixImageProps> = ({ image_id, ...props }) => {
       <div
         className={`flex items-center justify-center w-full h-full ${props?.className}`}
       >
-        <Spinner />
+        {image === "" ? (
+          <div className="flex items-center justify-center w-full h-full">
+            <ErrorIcon className="mr-2" />
+            <div>Image not found!</div>
+          </div>
+        ) : (
+          <Spinner />
+        )}
       </div>
     );
+
   return <img src={image as string} {...props} />;
 };
