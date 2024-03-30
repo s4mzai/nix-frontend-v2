53425b553cd78e7981f2df7ffa9c41644a939369
Vishal <dvishal485@gmail.com>
2024-03-30 10:16:16 +0530

    fix inheritance
    Signed-off-by: Vishal <dvishal485@gmail.com>

--- src/types/routeElement.ts
+++ src/types/routeElement.ts
@@ -1,8 +1,7 @@
+import { NonIndexRouteObject } from "react-router-dom";
 import Permission from "./permissions";
 
-interface RouteElement {
-  path: string;
-  element: React.ReactNode;
+interface RouteElement extends NonIndexRouteObject {
   permission: Permission[];
   label: string;
   icon?: React.SVGProps<SVGSVGElement>;
