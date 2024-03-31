ed9b791070be8df0cdea910bd2acff0e7177e380
Vishal <dvishal485@gmail.com>
2024-03-31 15:47:20 +0530

    nginx config such that no start scripts are required
    Signed-off-by: Vishal <dvishal485@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath } from "node:url";
+import process from "process";
 
 // https://vitejs.dev/config/
 export default defineConfig({
