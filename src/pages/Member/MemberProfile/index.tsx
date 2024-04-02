a9fd8e70950f50c4f8affd3f4f98b141b69f5132
Shobhit Tiwari <122107079+Shobhit141141@users.noreply.github.com>
2024-04-02 14:52:30 +0530

    member-profile-page route changed / issue sorted

--- src/pages/Member/MemberProfile/index.tsx
+++ src/pages/Member/MemberProfile/index.tsx
@@ -1,7 +1,7 @@
 import API from "@/services/API";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { CurrUserCtx } from "@/contexts/current_user";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { Spinner } from "@/components/Spinner";
 
 const AvatarImage = ({ src, alt, className }) => {
@@ -11,14 +11,49 @@ const AvatarImage = ({ src, alt, className }) => {
 const uri = API.getUri();
 
 export default function Profile() {
+  const { id } = useParams();
   const navigate = useNavigate();
   const { ready, user } = useContext(CurrUserCtx);
+  const [userDetails, setUserDetails] = useState({
+    name: "",
+    role: "",
+    email: "",
+    bio: ""
+  });
   if (!ready)
     return (
       <div className="flex w-full h-full justify-center items-center">
         <Spinner />
       </div>
     );
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const apiUrl = `/user/get-user/${id}`;
+        console.log("API URL:", apiUrl); // Logging the API URL
+        const response = await API.get(apiUrl);
+        const userData = response.data.data;
+        setUserDetails({
+          name: userData.name,
+          role: userData.role,
+          email: userData.email,
+          bio: userData.bio
+        });
+
+      } catch (error) {
+        console.error("Error fetching user details:", error);
+      }
+    };
+
+    if (id) {
+      fetchData();
+    }
+
+    return () => {
+      // Cleanup function 
+    };
+  }, [id]);
   return (
     <div className="max-w-4xl mx-auto my-10 p-8 shadow rounded">
       <div className="space-y-6 mt-4">
@@ -29,19 +64,19 @@ export default function Profile() {
                 <AvatarImage
                   alt={user.name}
                   className="h-36 w-36"
-                  src={`${API.getUri()}/images/get-avatar/${user.id}?thumbnail=true`}
+                  src={`${API.getUri()}/images/get-avatar/${id}?thumbnail=true`}
                 />
               </div>
               <div className="ms-4">
                 <h1 className="text-3xl font-semibold text-gray-800 font-sans">
-                  {user.name}
+                  {userDetails.name}
                 </h1>
-                <span className="text-gray-600">{user.role}</span>
+                <span className="text-gray-600">{userDetails.role}</span>
               </div>
             </div>
             <div>
               <Link
-                to={`/member/${user.id}/edit-details`}
+                to={`/member/edit-details/${id}/`}
                 className="bg-blue-500 text-white p-2 rounded hover:bg-green-500"
               >
                 Edit Info
@@ -50,25 +85,25 @@ export default function Profile() {
           </div>
         </div>
         <p className="mb-6 font-normal text-gray-600 text-lg">
-          {user.bio || "No bio available"}
+          {userDetails.bio || "No bio available"}
         </p>
         <hr className="border-t border-gray-300 mt-6 mb-6 w-full" />
 
         <ul className="mb-6">
           <li className="mb-2">
             <span className="text-gray-600 font-semibold">Email:</span>
-            <span className="text-gray-600 ml-1">{user.email}</span>
+            <span className="text-gray-600 ml-1">{userDetails.email}</span>
           </li>
         </ul>
 
-        <div>
+        {/* <div>
           <Link
-            to={`/member/edit-details/${user.id}/`}
+            to={`/member/edit-details/${id}/`}
             className="bg-blue-500 text-white p-2 rounded hover:bg-green-500"
           >
             Edit Info
           </Link>
-        </div>
+        </div> */}
       </div>
     </div>
   );
