986bbccf29ffbac380c80e5c62b88fec5e300338
Vishal <dvishal485@gmail.com>
2024-08-26 21:24:04 +0530

    update worker script
    Signed-off-by: Vishal <dvishal485@gmail.com>

--- public/notification-service.js
+++ public/notification-service.js
@@ -3,27 +3,13 @@
 /** @type {ServiceWorkerGlobalScope} */
 const service_worker = self;
 
-function urlB64ToUint8Array(base64String) {
-  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
-  const base64 = (base64String + padding).replace(/-/g, "+").replace(/_/g, "/");
-
-  const rawData = window.atob(base64);
-  const outputArray = new Uint8Array(rawData.length);
-  const outputData = outputArray.map((output, index) =>
-    rawData.charCodeAt(index),
-  );
-
-  return outputData;
-}
-
 service_worker.addEventListener("activate", async () => {
   console.log("Hello from service worker");
   try {
     /** @type {PushSubscriptionOptions} */
     const options = {
-      applicationServerKey: urlB64ToUint8Array(
+      applicationServerKey:
         "BCOsRaxpJeR0KyIPIg1rHx3pUtWVsGDGOxH65dDkqyU5ycF-CjPJxuqiXF4M0LpUMG_rk_YxSZX34uHbrV5umJQ",
-      ),
       userVisibleOnly: true,
     };
     console.log("Hello from service worker second time");
